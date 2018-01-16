a07bdacf175f820d6208f49b5f5b2a6be1c5628a
michalszymczak <34604917+michalszymczak@users.noreply.github.com>
2018-01-16 14:01:33 +0100

    wersja2

--- Michal_Szymczak/db.sh
+++ Michal_Szymczak/db.sh
@@ -1,6 +1,6 @@
 echo 'KONFIGURACJA BAZY'
 
-PASS="joomla"
+PASS="wordpress"
 export DEBIAN_FRONTEND=noninteractive
 
 echo 'AKTUALIZACJA UBUNTU'
@@ -11,10 +11,11 @@ sudo apt-get upgrade -y
 echo 'INSTALACJA, KONFIGURACJA MYSQL-SERVER'
 
 sudo -E apt-get -q -y install mysql-server
+netstat -tulpn |grep 3306
 sudo sed -i "s/.*bind-address.*/bind-address = 0.0.0.0/" /etc/mysql/my.cnf
 sudo service mysql restart
 mysqladmin -u root password $PASS
-mysql -uroot -p$PASS -e "create database wordpress"
-mysql -uroot -p$PASS -e "GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, INDEX, ALTER, CREATE TEMPORARY TABLES ON wordpress.* TO 'wordpress'@'%' IDENTIFIED BY 'password';"
+mysql -uroot -p$PASS -e "CREATE DATABASE wordpress;"
+mysql -uroot -p$PASS -e "GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, INDEX, ALTER, CREATE TEMPORARY TABLES ON wordpress.* to 'wordpress'@'%' IDENTIFIED BY 'wordpress';"
 
 echo 'KONIEC'
